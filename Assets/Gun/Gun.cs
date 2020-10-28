34b8c74afc67af1ca7dd80b573d728f235a8fb77
Egor <garemexpo@gmail.com>
2020-10-28 13:50:56 +0300

    Gun animations with layers

---
+++ Assets/Gun/Gun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    private Animator _animator;
+    
+    [SerializeField] private AnimatorOverrideController  _alterniteAimAnimatorContronller;
+    private RuntimeAnimatorController  _cacheController;
+    
+    void Start() {
+        _animator = GetComponent<Animator>();
+        _cacheController = _animator.runtimeAnimatorController;
+    }
+
+    
+    void Update()
+    {
+        if (Input.anyKeyDown == false) {
+            _animator.SetBool("Move", false);
+            _animator.SetBool("Shoot", false);
+            _animator.SetBool("Aim", false);
+        }
+        
+        
+        if (Input.GetKey(KeyCode.Mouse1)) {
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                _animator.runtimeAnimatorController = _alterniteAimAnimatorContronller;
+            } else _animator.runtimeAnimatorController = _cacheController;
+            
+            _animator.SetBool("Aim", true);
+
+        }
+        
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            _animator.SetBool("Shoot", true);
+        }
+        
+        if (Input.GetKey(KeyCode.W)) {
+            _animator.SetBool("Move", true);
+        }
+    }
+
+    public void Shoot() {
+        Debug.Log("Shoot");
+    }
+}
